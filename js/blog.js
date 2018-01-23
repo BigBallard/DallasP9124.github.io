33df02f9313ad8c91865c9bfcce70b506f8029d7
Dallas Phillips <dallas.phillips@wsu.edu>
2018-01-22 22:08:00 -0800

    jquery tesing

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,7 @@
-function hideToggle(elementId){
-  var x = document.getElementById(elementId)
-  alert(elementId)
-  if(x.style.display === "none"){
-    x.style.display = "block"
-  }else{
-    x.style.display = "none"
-  }
-}
-
 $(function(){
-  $("#current").load("../../posts/post1.html")
+  $(".dropdown-item").click(function(){
+    var post = $(this).attr("post")
+    var url = "../../post/" + post + ".html"
+    $("#current").load(url)
+  })
 })

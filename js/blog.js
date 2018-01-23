121719623385a1d10c72183b691aa362dada9a0b
Dallas Phillips <dallas.phillips@wsu.edu>
2018-01-22 18:20:58 -0800

    checking if ajax loads relative off local

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,13 @@
-function myFunc(){
-  alert("asdfasdf")
+function hideToggle(elementId){
+  var x = document.getElementById(elementId)
+  alert(elementId)
+  if(x.style.display === "none"){
+    x.style.display = "block"
+  }else{
+    x.style.display = "none"
+  }
 }
+
+$(function(){
+  $("#current").load("../posts/post1.html")
+})

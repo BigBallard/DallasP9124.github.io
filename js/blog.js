025db0ecf41a0d1e37d8b839579d117442f0e086
root <root@EREBUS.localdomain>
2018-10-24 17:50:59 -0700

    setting under construction

--- js/blog.js
+++
@@ -1,8 +0,0 @@
-function loadContent(post){
-    var url = "../../posts/" + post + ".html"
-    $("#current").load(url)
-}
-
-$( document ).ready(function() {
-  $("#current").load("../../posts/post13.html")
-});
